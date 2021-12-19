7acb46b2d2ba0b2443fc06e0db28ea21422f31f4
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2021-12-19 13:14:02 +0530

    Create Practical11_Task2.cpp

---
+++ Practical11/Practical11_Task2.cpp
@@ -0,0 +1,52 @@
+/*Multiple Inheritance in C++ :
+    Create two classes named Mammals and MarineAnimals. Create another class named BlueWhale which inherits both the above classes. Now, create a function in each of these
+    classes which prints "I am Mammal", "I am a Marine Aniimal" and "I belong to both the categories : Mammals as well as Marine Animals" respectively. Now, create an object
+    for each of the above and try calling :
+    
+    1. function of Mammals by an object of Mammal
+    2. 
+    
+*/
+
+#include<iostream>
+using namespace std;
+
+class Mammals
+{
+public:
+	void displayMammals()
+    {
+        cout<<"I am Mammal"<<endl;
+    }
+};
+
+class MarineAnimals
+{
+public:
+	void displayMarine()
+    {
+        cout<<"I am a Marine Animal"<<endl;
+    }
+};
+
+class BlueWhale : public Mammals, public MarineAnimals
+{
+public:
+	void displayBoth()
+    {
+        cout<<"I belong to both the categories : Mammals as well as Marine Animals"<<endl;
+    }
+};
+
+int main()
+{
+	Mammals A;
+	MarineAnimals B;
+	BlueWhale C;
+	A.displayMammals();
+	B.displayMarine();
+	C.displayBoth();
+	C.displayMammals();
+	C.displayMarine();
+return 0;
+}
