44577f43f4ee83a3a10c6fac153e6a034c4a6ba6
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-01-09 22:44:19 +0530

    Create Practical17_Task2(VECTOR).cpp

---
+++ Practical17/Practical17_Task2(VECTOR).cpp
@@ -0,0 +1,9 @@
+/* WAP in C++ with the help of STL:
+    b. Vector:
+        1. Insert elements into a int vector
+        2. Iterate this vector using iterator and print it
+        3. Find size of a capacity and max size of a vector
+        4. Resize a vector
+        5. checks if the vector is empty or no
+ */
+
